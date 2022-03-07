0a33e43781a33e8fc631d7c4e059419fd50ebc99
pixiepucker <autumnoffic@gmail.com>
2022-03-06 22:10:06 -0600

    added due date auditing

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  //check duedate
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -182,6 +184,9 @@ $('.list-group').on("change", "input[type='text']", function () {
 
   //replace input with span element
   $(this).replaceWith(taskSpan);
+
+  //pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest('.list-group-item'));
 });
 
 $('.card .list-group').sortable({
@@ -258,6 +263,26 @@ $('#trash').droppable({
 $('#modalDueDate').datepicker({
   minDate: 1
 });
+
+
+var auditTask= function(taskEl) {
+  //get date from task element
+  var date = $(taskEl).find('span').text().trim();
+
+  //convert to moment object at 1700
+  var time = moment(date, 'L').set('hour', 17);
+
+  //remove old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  //apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } else if
+    (Math.abs(moment().diff(time,'days')) <= 2) {
+      $(taskEl).addClass("list-group-item-warning");
+    }
+  };
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
