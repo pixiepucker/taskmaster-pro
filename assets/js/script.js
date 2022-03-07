d7d945e73fd89851cf226e7f67c1eb31ac1a50c1
pixiepucker <autumnoffic@gmail.com>
2022-03-07 00:11:10 -0600

    added styling to buttons and transitions to remove task

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -195,16 +195,18 @@ $('.card .list-group').sortable({
   tolerance: 'pointer',
   helper: 'clone',
   activate: function(event) {
-    console.log('activate', this);
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
-    console.log('deativate', this)
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log('over', event.target);
+    $(event.target).addClass('dropover-active');
   },
   out: function(event) {
-    console.log('out', event.target);
+    $(event.target).removeClass('dropover-active');
   },
   update: function(event) {
 
@@ -253,9 +255,11 @@ $('#trash').droppable({
   },
   over: function(event, ui) {
     console.log('over');
+    $('.bottom-trash').addClass('bottom-trash-active');
   },
   out: function (event, ui) {
     console.log('out');
+    $('.bottom-trash').removeClass("bottom-trash-active");
   }
 });
 
@@ -282,6 +286,7 @@ var auditTask= function(taskEl) {
     (Math.abs(moment().diff(time,'days')) <= 2) {
       $(taskEl).addClass("list-group-item-warning");
     }
+  console.log(taskEl);
   };
 
 // remove all tasks
@@ -295,3 +300,9 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function(){
+  $('.card .list-group-item').each(function(index,el) {
+    auditTask(el);
+  });
+}, 1800000);
