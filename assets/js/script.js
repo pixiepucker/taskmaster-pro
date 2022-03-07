a3ac88d118e04760ae733c2b1acfed77b603fec9
pixiepucker <autumnoffic@gmail.com>
2022-03-06 21:38:29 -0600

    added datepicker to modal and tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,12 +140,21 @@ $('.list-group').on('click', 'span', function () {
   //swap out elements
   $(this).replaceWith(dateInput);
 
+  //enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function () {
+      //when calendar is closed, force a "change" event on the 'dateInput'
+      $(this).trigger('change');
+    }
+  });
+
   //automatically focus on new element
   dateInput.trigger("focus");
 });
 
 //value of due date was changed
-$('.list-group').on("blur", "input[type='text']", function () {
+$('.list-group').on("change", "input[type='text']", function () {
   //get current text 
   var date = $(this)
   .val()
@@ -243,6 +252,11 @@ $('#trash').droppable({
   out: function (event, ui) {
     console.log('out');
   }
+});
+
+//adds datepicker to modal
+$('#modalDueDate').datepicker({
+  minDate: 1
 });
 
 // remove all tasks
